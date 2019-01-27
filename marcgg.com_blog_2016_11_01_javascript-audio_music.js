f6b6f908b2a61981df6b38eaed3b36c84031f858
abubelinha <abubelinha@yahoo.es>
2019-01-27 17:35:29 +0100

    Update marcgg.com_blog_2016_11_01_javascript-audio_music.js

--- marcgg.com_blog_2016_11_01_javascript-audio_music.js
+++ marcgg.com_blog_2016_11_01_javascript-audio_music.js
@@ -28,30 +28,40 @@ function loadAudio() {
 	,false);
 }
 
-function example1(){o=context.createOscillator()
-o.type="sine"
-o.connect(context.destination)
-o.start()}
-function example2(){o=context.createOscillator()
-g=context.createGain()
-o.connect(g)
-g.connect(context.destination)
-o.start(0)}
-function example2Stop(decreaseTime){g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime+decreaseTime)}
-function example3(type,x){o=context.createOscillator()
-g=context.createGain()
-o.connect(g)
-o.type=type
-g.connect(context.destination)
-o.start(0)
-g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime+x)}
-function example4(frequency,type){ return;
+function example1(){
+	o=context.createOscillator()
+	o.type="sine"
+	o.connect(context.destination)
+	o.start()
+}
+function example2(){
+	o=context.createOscillator()
+	g=context.createGain()
+	o.connect(g)
+	g.connect(context.destination)
+	o.start(0)
+}
+function example2Stop(decreaseTime){
+	g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime+decreaseTime)
+}
+function example3(type,x){
+	o=context.createOscillator()
+	g=context.createGain()
+	o.connect(g)
+	o.type=type
+	g.connect(context.destination)
+	o.start(0)
+	g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime+x)
+}
+function example4(frequency,type){ 
+//return;
 	if(o===null) loadAudio();
-o=context.createOscillator()
-g=context.createGain()
-o.type=type
-o.connect(g)
-o.frequency.value=frequency
-g.connect(context.destination)
-o.start(0)
-g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime+1)}
+	o=context.createOscillator()
+	g=context.createGain()
+	o.type=type
+	o.connect(g)
+	o.frequency.value=frequency
+	g.connect(context.destination)
+	o.start(0)
+	g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime+1)
+}
